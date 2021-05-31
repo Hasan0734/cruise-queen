4faf84412315ce2ec250dab9520a62d170bae836
Hasan07 <Jahidhasan2856@gmail.com>
2021-05-31 16:40:15 +0600

    some update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 function handleTicket(ticketClass, isIncrease) {
-    
-    const ticketCount = getInputValue(ticketClass)
+
+    const ticketCount = getInputValue(ticketClass);
     let ticketNewCount = ticketCount
     if (isIncrease == true) {
         ticketNewCount = ticketCount + 1;
@@ -9,23 +9,23 @@ function handleTicket(ticketClass, isIncrease) {
     if (isIncrease == false && ticketCount > 0) {
         ticketNewCount = ticketCount - 1;
     }
-    document.getElementById(ticketClass + '-ticket').value = ticketNewCount
-   totalCalculate()
+    document.getElementById(ticketClass + '-ticket').value = ticketNewCount;
+    totalCalculate();
 }
 
-function totalCalculate(){
-    const firstClassCount = getInputValue('first')
-    const economyClassCount = getInputValue('economy')
+function totalCalculate() {
+    const firstClassCount = getInputValue('first');
+    const economyClassCount = getInputValue('economy');
     const totalPrice = firstClassCount * 150 + economyClassCount * 100;
     document.getElementById('subtotal').innerText = '$' + totalPrice;
     const vat = totalPrice * (10 / 100);
     document.getElementById('vat').innerText = '$' + vat;
-    const grandTotal = totalPrice + vat
-    document.getElementById('grand-total').innerText ='$' + grandTotal
+    const grandTotal = totalPrice + vat;
+    document.getElementById('grand-total').innerText = '$' + grandTotal;
 }
 
 function getInputValue(ticketClass) {
     const inputTicket = document.getElementById(ticketClass + '-ticket')
-    const ticketCount = parseInt(inputTicket.value)
+    const ticketCount = parseInt(inputTicket.value);
     return ticketCount;
 }
